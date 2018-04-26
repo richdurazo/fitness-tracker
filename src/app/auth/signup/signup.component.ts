37612bac524ca979b28e963e674d56f24d1575d5
Richard Durazo <rcdurazo@gmail.com>
2018-04-25 17:43:37 -0700

    feature/style: add material components and theme, complete login and signup forms, add navigation

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+import * as _moment from 'moment';
+import * as _rollupMoment from 'moment';
+const moment = _rollupMoment || _moment;
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
+  maxDate;
   constructor() { }
 
   ngOnInit() {
+    this.maxDate = moment().subtract(18, 'years');
   }
   onSubmit(form: NgForm) {
+
     console.log(form)
   }
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,16 +2,37 @@ import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatMomentDateModule} from '@angular/material-moment-adapter';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatMenuModule, MatCardModule } from '@angular/material';
+
 @NgModule({
     imports: [
         MatButtonModule,
         MatIconModule,
-        MatInputModule
+        MatInputModule,
+        MatDatepickerModule,
+        MatMomentDateModule,
+        MatCheckboxModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatMenuModule,
+        MatCardModule
     ],
     exports: [
         MatButtonModule,
         MatIconModule,
-        MatInputModule
+        MatInputModule,
+        MatDatepickerModule,
+        MatMomentDateModule,
+        MatCheckboxModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatMenuModule,
+        MatCardModule
     ]
 })
 export class MaterialModule {}
